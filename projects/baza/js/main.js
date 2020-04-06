35cc633b36f18234722fb9aad2510fca7ad84722
Pavel Pullso <p.dubitskiy@ya.ru>
2020-04-06 21:03:07 +0500

    add map, sliders

--- projects/baza/js/main.js
+++ projects/baza/js/main.js
@@ -1,10 +1,39 @@
 $(document).ready(function () {
   $('.life-slider').slick({
     centerMode: true,
-    centerPadding: '0px',
+    centerPadding: '10px',
     slidesToShow: 3,
     arrows: false,
     autoplay: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+  $('.partners-slider').slick({
+    slidesToShow: 3,
+    arrows: true,
+    prevArrow: '<button class="slick-prev"></button>',
+    nextArrow: '<button class="slick-next"></button>',
+    autoplay: true,
+    dots: true,
+    dotsClass: 'dots-style',
     responsive: [
       {
         breakpoint: 768,
